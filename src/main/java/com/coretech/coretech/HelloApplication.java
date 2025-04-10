583d750275952d2c65fb0a64ab1f33d70feb9f67
frinx019 <frinpatel1841974@gmail.com>
2025-04-10 18:13:15 -0400

    Admin Dashboard table created and display recent appointment from database

--- src/main/java/com/coretech/coretech/HelloApplication.java
+++ src/main/java/com/coretech/coretech/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainVehicleManagement.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("AdminView.fxml"));
         Parent root = fxmlLoader.load();
 
         // Get the controller and set the stage reference
